556cc105db2231ff9c1adec77bc25c63d50e781c
ninjagoku4560 <nathangabard@outlook.fr>
2023-10-28 11:39:06 +0200

    commit 0.1.0

--- src/main/java/fr/ninjagoku4560/iseeitall/Iseeitall.java
+++ src/main/java/fr/ninjagoku4560/iseeitall/Iseeitall.java
@@ -2,24 +2,31 @@ package fr.ninjagoku4560.iseeitall;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
-import org.apache.logging.log4j.LogManager;
 
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Iseeitall implements ModInitializer {
     /**
      * Runs the mod initializer.
      */
-    private static final Logger LOGGER = (Logger) LogManager.getLogger("Iseeitall");
+    public static Logger LOGGER = LogManager.getLogger("Iseeitall");
     @Override
     public void onInitialize() {
         LOGGER.info("Initialization of the ISIA mod server side");
 
+        // Player break a block
         AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
             LOGGER.info("The player "+player.getName().toString()+" to break " + pos.toString() + "in " + world.toString());
+            return ActionResult.PASS;
+        });
+        // Player hit other entity
+        AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+            LOGGER.info(player.getName().toString() + " hit " + getEntityName(entity));
             return ActionResult.PASS;
         });
     }
--- src/main/java/fr/ninjagoku4560/iseeitall/client/IseeitallClient.java
+++ src/main/java/fr/ninjagoku4560/iseeitall/client/IseeitallClient.java
@@ -1,13 +1,15 @@
 package fr.ninjagoku4560.iseeitall.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import fr.ninjagoku4560.iseeitall.Iseeitall;
 
 public class IseeitallClient implements ClientModInitializer {
+
     /**
      * Runs the mod initializer on the client environment.
      */
     @Override
     public void onInitializeClient() {
-
+        Iseeitall.LOGGER.info("Client Initialize started...");
     }
 }
