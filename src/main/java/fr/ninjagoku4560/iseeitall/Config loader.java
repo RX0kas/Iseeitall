dc53c0ca4e3555f2837c998e68081c8239a83af6
ninjagoku4560 <116596186+ninjagoku4560@users.noreply.github.com>
2023-11-01 09:58:52 +0100

    Update Config loader.java

--- src/main/java/fr/ninjagoku4560/iseeitall/Config loader.java
+++ src/main/java/fr/ninjagoku4560/iseeitall/Config loader.java
@@ -1,1 +1,20 @@
-package fr.ninjagoku4560.iseeitall.ConfigLoader
+package fr.ninjagoku4560.iseeitall
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ConfigLoader {
+    try:
+        String configPath = "config/config.json";
+    // Lecture du fichier JSON
+        JsonObject config = new Gson().fromJson(new FileReader(configPath), JsonObject.class);
+    } catch (IOException e {e.printStackTrace();}
+
+    public static bool getBoolConfig(String parameter) {
+        boolean data = config.get(parameter).getAsBoolean();
+        return data
+    }
+
+}
+
